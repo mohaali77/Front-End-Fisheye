9a65619b676fba1cb82e06d41fb0521bd05f0aa1
mohaali77 <mzr.mohand@gmail.com>
2023-01-16 18:22:53 +0100

    modif

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,27 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+        const city_country = document.createElement('div')
+        city_country.textContent = city + ', ' + country;
+        const tagLine = document.createElement('div');
+        tagLine.textContent = tagline;
+        const textPrice = document.createElement('div');
+        textPrice.textContent = price + ' €'
+
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(city_country);
+        article.appendChild(tagLine);
+        article.appendChild(textPrice);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,8 +1,10 @@
+//ouvre la fenêtre contactez-moi
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    modal.style.display = "block";
 }
 
+//ferme la fenêtre contactez-moi
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
